d09f6e6442a0b263d8025f9b25e631763cc24ec0
JisooPyo <audwl326@gmail.com>
2023-08-04 00:52:59 +0900

    User Entity 구현

--- src/main/java/sparta/blogfinal/user/entity/User.java
+++ src/main/java/sparta/blogfinal/user/entity/User.java
@@ -1,4 +1,33 @@
 package sparta.blogfinal.user.entity;
 
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false, unique = true)
+	private String email;
+	@Column(nullable = false)
+	private String nickname;
+	@Column(nullable = false)
+	private String password;
+	@Column(nullable = false)
+	private LocalDate birthday;
+	@Column(nullable = false)
+	@Enumerated(value = EnumType.STRING)
+	private UserRoleEnum role;
+	private String introduction;
+	private Long kakaoId;
+	@CreatedDate
+	@Column(updatable = false)
+	@Temporal(TemporalType.DATE)
+	private LocalDate createdAt;
+
+
 }
--- src/main/java/sparta/blogfinal/user/entity/UserRoleEnum.java
+++ src/main/java/sparta/blogfinal/user/entity/UserRoleEnum.java
@@ -1,4 +1,22 @@
 package sparta.blogfinal.user.entity;
 
-public class UserRoleEnum {
+public enum UserRoleEnum {
+	USER(Authority.USER),  // 사용자 권한
+	ADMIN(Authority.ADMIN);  // 관리자 권한
+
+	private final String authority;
+
+	UserRoleEnum(String authority) {
+		this.authority = authority;
+	}
+
+	public String getAuthority() {
+		return this.authority;
+	}
+
+	public static class Authority {
+		// 권한 앞에 "ROLE_"을 붙이는 것이 규칙이다!
+		public static final String USER = "ROLE_USER";
+		public static final String ADMIN = "ROLE_ADMIN";
+	}
 }
