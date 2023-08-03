b57d90096bff3ba0001032c20ae3c430c9a96a03
JisooPyo <audwl326@gmail.com>
2023-08-04 01:09:57 +0900

    UserRepository 구현

--- src/main/java/sparta/blogfinal/user/repository/UserRepository.java
+++ src/main/java/sparta/blogfinal/user/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package sparta.blogfinal.user.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import sparta.blogfinal.user.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
