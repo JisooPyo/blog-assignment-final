c01ea519f2e841ed72ff42d868f517993b6c5233
JisooPyo <audwl326@gmail.com>
2023-08-04 01:09:46 +0900

    SignupRequestDto 구현

--- src/main/java/sparta/blogfinal/user/dto/SignupRequestDto.java
+++ src/main/java/sparta/blogfinal/user/dto/SignupRequestDto.java
@@ -1,4 +1,32 @@
 package sparta.blogfinal.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
 public class SignupRequestDto {
+	@Email
+	@NotBlank
+	private String email;
+
+	@NotBlank
+	private String nickname;
+
+	@NotBlank
+	@Size(min = 8, max = 15, message = "비밀번호는 최소 8자 이상, 15자 이하여야 합니다.")
+	@Pattern(regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]*$", message = "비밀번호는 알파벳 대소문자(a~z, A~Z), 숫자(0~9), 특수문자로 구성되어야 합니다.")
+	private String password;
+
+	@NotBlank
+	private String birthday; // 20230804
+
+	private String introduction;
+
+	private boolean admin = false;
+	private String adminToken = "";
 }
