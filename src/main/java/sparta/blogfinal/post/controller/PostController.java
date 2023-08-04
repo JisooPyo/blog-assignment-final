0b1a834133293da3e4ec1623791467e57cf8cf2d
JisooPyo <audwl326@gmail.com>
2023-08-04 12:35:39 +0900

    Post CRUD 틀 구현

---
+++ src/main/java/sparta/blogfinal/post/controller/PostController.java
@@ -0,0 +1,59 @@
+package sparta.blogfinal.post.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import sparta.blogfinal.common.dto.ApiResponseDto;
+import sparta.blogfinal.common.security.UserDetailsImpl;
+import sparta.blogfinal.post.dto.PostRequestDto;
+import sparta.blogfinal.post.dto.PostResponseDto;
+import sparta.blogfinal.post.entity.Post;
+import sparta.blogfinal.post.service.PostService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class PostController {
+
+	private final PostService postService;
+
+	// 포스트 작성
+	@PostMapping("/posts")
+	public ResponseEntity<PostResponseDto> createPost(@AuthenticationPrincipal UserDetailsImpl userDetails,
+													  @RequestBody PostRequestDto requestDto){
+		return null;
+	}
+
+	// 전체 포스트 조회
+	@GetMapping("/posts")
+	public ResponseEntity<List<PostResponseDto>> getPosts() {
+		return null;
+	}
+
+	// 선택 포스트 조회
+	@GetMapping("/posts/{id}")
+	public ResponseEntity<PostResponseDto> getPostById(@PathVariable Long id){
+		return null;
+	}
+
+	// 포스트 수정
+	@PutMapping("/posts/{id}")
+	public ResponseEntity<PostResponseDto> updatePost(@AuthenticationPrincipal UserDetailsImpl userDetails,
+													 @PathVariable Long id,
+													 @RequestBody PostRequestDto requestDto){
+		return null;
+	}
+
+	// 포스트 삭제
+	@DeleteMapping("/posts/{id}")
+	public ResponseEntity<ApiResponseDto> deletePost(@AuthenticationPrincipal UserDetailsImpl userDetails,
+													 @PathVariable Long id){
+		return null;
+	}
+
+
+
+}
---
+++ src/main/java/sparta/blogfinal/post/service/PostService.java
@@ -0,0 +1,9 @@
+package sparta.blogfinal.post.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+}
