ce373d403a2b088fc1ffbcfe4c495981876139b8
JisooPyo <audwl326@gmail.com>
2023-08-04 11:21:19 +0900

    [update] UserController @RestController로 변경
    +) signup 메서드에서 잘못된 필드가 있으면 프론트단에 알려주기

--- src/main/java/sparta/blogfinal/user/controller/UserController.java
+++ src/main/java/sparta/blogfinal/user/controller/UserController.java
@@ -5,11 +5,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import sparta.blogfinal.common.dto.ApiResponseDto;
 import sparta.blogfinal.user.dto.SignupRequestDto;
 import sparta.blogfinal.user.service.UserService;
@@ -17,22 +18,21 @@ import sparta.blogfinal.user.service.UserService;
 import java.util.List;
 
 @Slf4j
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class UserController {
 	private final UserService userService;
 
 	@PostMapping("/user/signup")
-	public ResponseEntity<ApiResponseDto> signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+	public ResponseEntity<ApiResponseDto> signup(@Valid @RequestBody SignupRequestDto requestDto, BindingResult bindingResult) {
 		List<FieldError> fieldErrors = bindingResult.getFieldErrors();
 		if (fieldErrors.size() > 0) {
 			for (FieldError fieldError : bindingResult.getFieldErrors()) {
 				log.error(fieldError.getField() + "필드 : " + fieldError.getDefaultMessage());
 			}
-			throw new IllegalArgumentException("잘못된 항목이 있습니다.");
+			return ResponseEntity.badRequest().body(new ApiResponseDto("잘못된 필드가 있습니다.", HttpStatus.BAD_REQUEST.value()));
 		}
-
 		userService.signup(requestDto);
 		return ResponseEntity.ok().body(new ApiResponseDto("회원가입 성공", HttpStatus.OK.value()));
 	}
