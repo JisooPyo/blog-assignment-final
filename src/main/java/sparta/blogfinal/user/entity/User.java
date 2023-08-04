e7d9f10594204c7d7d422a7601396aa86ed4617e
JisooPyo <audwl326@gmail.com>
2023-08-04 10:21:46 +0900

    [update] User @NoArgsConstructor 어노테이션 추가

--- src/main/java/sparta/blogfinal/user/entity/User.java
+++ src/main/java/sparta/blogfinal/user/entity/User.java
@@ -2,6 +2,7 @@ package sparta.blogfinal.user.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -10,6 +11,7 @@ import java.time.LocalDate;
 @EntityListeners(AuditingEntityListener.class)
 @Entity
 @Getter
+@NoArgsConstructor
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
