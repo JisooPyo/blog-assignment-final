db47c06d63f4361a5db560728b5122537f55a881
JisooPyo <audwl326@gmail.com>
2023-08-04 09:05:28 +0900

    User @Getter 추가

--- src/main/java/sparta/blogfinal/user/entity/User.java
+++ src/main/java/sparta/blogfinal/user/entity/User.java
@@ -1,12 +1,14 @@
 package sparta.blogfinal.user.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 @EntityListeners(AuditingEntityListener.class)
 @Entity
+@Getter
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
