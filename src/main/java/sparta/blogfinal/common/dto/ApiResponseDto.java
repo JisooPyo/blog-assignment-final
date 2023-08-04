7b96f0f5f08f093802c58504a085a5913726799d
JisooPyo <audwl326@gmail.com>
2023-08-04 11:14:44 +0900

    [fix] ResponseEntity return 안되는 문제 수정
    ApiResponseDto에 Getter 추가해서 해결

--- src/main/java/sparta/blogfinal/common/dto/ApiResponseDto.java
+++ src/main/java/sparta/blogfinal/common/dto/ApiResponseDto.java
@@ -1,11 +1,12 @@
 package sparta.blogfinal.common.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @NoArgsConstructor
+@Getter
 public class ApiResponseDto {
 	private String msg;
 	private Integer statusCode;
